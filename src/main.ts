acf43813df0e4b372f0a5454456c48b6bce5b6d8
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2024-01-03 01:56:36 -0300

    Add Biomejs to formatter project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import fastify from 'fastify'
-import config from './config'
-import { registerPlugins } from './plugins'
 import routes from './app.module'
 import clusterize from './clusterize'
+import config from './config'
+import { registerPlugins } from './plugins'
 
 const server = fastify({
   logger: true
@@ -10,7 +10,6 @@ const server = fastify({
 
 async function bootstrap() {
   try {
-    //server.setErrorHandler((error, request, reply) =>   (error, request, reply));
     registerPlugins(server, config);
     server.register(routes, { prefix: config.stripPrefix.path })
     server.listen({ port: config.app.port || 3000 })
