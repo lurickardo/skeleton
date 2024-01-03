603483fc20b79476eb1ea9afabd3990f75bbaac2
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2024-01-03 19:34:24 -0300

    fix: remove force error

--- src/v1/modules/user/controller/user.controller.ts
+++ src/v1/modules/user/controller/user.controller.ts
@@ -3,7 +3,6 @@ import userService from "../service/user.service";
 
 export default {
 	findUser: async ({ params: { id } }, reply: FastifyReply) => {
-		throw Error('safsa');
 		return reply.code(200).send(await userService.findUser(id));
 	},
 };
