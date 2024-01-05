fa79b37952cfbfa5dc56b6e7c3dcb28016f6ae7f
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2024-01-04 23:41:09 -0300

    fix: add tests to create route

--- src/v1/modules/user/user.service.spec.ts
+++ src/v1/modules/user/user.service.spec.ts
@@ -7,4 +7,14 @@ describe("Unit tests for user service", () => {
 			expect(result).toEqual({ idUser: "1" });
 		});
 	});
+	
+
+	describe("create tests", () => {
+		it("should have return correctly", async () => {
+			const result = await userService.create({name: 'foo', email: 'bar'});
+			expect(result).toBeInstanceOf(Object)
+			expect(result).toHaveProperty('name')
+			expect(result).toHaveProperty('email')
+		});
+	});
 });
--- src/v1/modules/user/user.service.ts
+++ src/v1/modules/user/user.service.ts
@@ -1,8 +1,12 @@
+import * as HttpStatus from "http-status";
+import { httpException } from "../../../../src/config/error";
 import { createUserDto } from "./dto/create.dto";
 
 export const userService = {
 	findById: async (id: string) => {
 		try {
+			if(!id)
+				throw httpException('Id user not found.', HttpStatus.NOT_FOUND)
 			return { idUser: id };
 		} catch (error) {}
 	},
