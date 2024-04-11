ebec68936d0273ec8862483d60f63bfe18944023
Lurickardo <luizr726@gmail.com>
2024-04-11 01:28:59 -0300

    fix: add message of send notification success

--- src/v1/modules/notification/notification.service.ts
+++ src/v1/modules/notification/notification.service.ts
@@ -4,8 +4,10 @@ import { SendNotificationDto } from "./dto/sendNotification.dto";
 export const notificationService = {
 	sendNotification: async (sendNotificationDto: SendNotificationDto) => {
 		console.info(sendNotificationDto);
+		console.info("Sending notification successfully!")
 	},
 	sendNotifications: async (sendNotificationsDto: SendNotificationsDto) => {
 		console.info(sendNotificationsDto);
+		console.info("Sending notifications successfully!")
 	},
 };
